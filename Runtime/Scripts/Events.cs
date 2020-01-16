bed71131440ed8fdb022b0f2c63afb1d2573637c
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-01-16 13:40:45 +0900

    Populate with the code already made so far

---
+++ Runtime/Scripts/Events.cs
@@ -0,0 +1,9 @@
+﻿using System;   //Serializable
+using UnityEngine.Events;
+
+public static class Events {
+    //Fadeout: true. Fadein: false
+    [Serializable] public class MenuFadeComplete : UnityEvent<bool> { }
+
+    [Serializable] public class GameStateChanged : UnityEvent<GameState, GameState> { }
+}
---
+++ Runtime/Scripts/GameBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameBehaviour : MonoBehaviour {
+
+    protected virtual void Awake() {
+        m_transform = transform;
+    }
+    
+//---------------------------------------------------------------------------------------------------------------------
+    protected Transform m_transform = null;
+}
